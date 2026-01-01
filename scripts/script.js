40a3850ddaa80f69ba180068d66701f302e4bf63
zibuyin <natdrone101@gmail.com>
2026-01-01 12:10:15 +0000

    Fixed footer position

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+// Defining Elements
+
+const footer = document.getElementById("footer")
+
+function isPageOverflowing() {
+    return document.documentElement.scrollHeight > window.innerHeight;
+}
+
+function updateFooterPosition() {
+    if (isPageOverflowing()) {
+        footer.style.position = "relative"
+        console.log("overflowed")
+    }
+    else{
+        footer.style.position = "fixed"
+        console.log("not overflowing")
+    }
+}
+
+updateFooterPosition();
+window.addEventListener("resize", updateFooterPosition);
