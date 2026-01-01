c4e119a781df7cc7110331f6c557f6e8b7779ab0
zibuyin <natdrone101@gmail.com>
2026-01-01 15:47:10 +0000

    Adeed badge

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,8 +8,38 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// CONSTANTS
+const HACKATIME_URL = "https://hackatime.hackclub.com/api/v1/stats"
+
+async function getHackatimeResponse(env){
+	const apiKey = env.HACKATIME_API_KEY
+	const url = HACKATIME_URL + "?api_key=" + apiKey
+	try{
+		const response = await fetch(url)
+		if(!response.ok){
+			throw new Error(`Response status: ${response.status}`);
+		}
+		const result = await response.json()
+		return result
+	}
+	catch (error){
+		console.error(error.message)	
+		return {error: error.message}
+	}
+	
+
+}
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const url = new URL(request.url)
+
+		if (url.pathname === "/api/v1/hackatime"){
+			const data = await getHackatimeResponse(env);
+			return new Response(JSON.stringify(data));
+		}
+		else{
+			return new Response("Cannot GET from URL")
+		}
+		
 	},
 };
