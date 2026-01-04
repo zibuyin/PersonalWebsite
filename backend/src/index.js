6961f15c7031cfad24a3a4403d8a3e6786fcae92
zibuyin <natdrone101@gmail.com>
2026-01-04 20:00:35 +0000

    test(backend): add ip testing script

--- backend/src/index.js
+++ backend/src/index.js
@@ -85,7 +85,10 @@ async function hashString(inputString) {
 
 async function handleUniqueUser(request, env){
 	const ip = request.headers.get("CF-Connecting-IP")
-	const hash = (await hashString(ip)).toString();
+	// const hash = (await hashString(ip)).toString();
+
+	// REMOVE IN PRODUCTION [TODO]
+	const hash = ip
 	try {
 		await env.DB
 			.prepare(
